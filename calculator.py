9b7b351ac2fc7fe6ec5bff9d5918aa6d9d84149e
Charles Omukaya <charlesomukaya3@gmail.com>
2025-11-20 23:48:14 +0300

    Resolve merge conflict and add subtract function

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 def add(a, b):
   return a + b
-=======
-def add(a, b):\n return a + b
-def subtract(a, b):\n   return a - b
->>>>>>> 56bd971 (Add subtract function)
+  
+def subtract(a, b):   
+  return a - b
